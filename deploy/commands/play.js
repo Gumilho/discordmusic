f4dcee6388afc3328dc4f4754852b6c8a6166b7a
Daniel Yan <doraniel2000@gmail.com>
2023-06-04 16:10:31 -0300

    Edit deploy

--- deploy/commands/play.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = new SlashCommandBuilder()
-   .setName('play')
-   .setDescription('Toca a musica')
-   .addStringOption(option => option.setName('query')
-       .setRequired(true)
-       .setDescription('Nome da musica ou link do youtube')
-   )
